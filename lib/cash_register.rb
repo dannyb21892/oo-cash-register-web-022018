4a688f451e57edd09a3491934970382382f5787f
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-21 21:53:20 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity=1)
-    @items << title if !@items.include?(title) end
+    @items << title if !@items.include?(title)
     @total += price*quantity
   end
     
